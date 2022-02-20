e3bb069ac7531489918d6e56e9062e220c0c4ed0
amit yosefi <amit.yosefi@gmail.com>
2022-02-20 13:30:35 +0200

    Updates

--- src/hooks/index.js
+++
@@ -1,3 +0,0 @@
-export { usePeopleFetch } from "./usePeopleFetch";
-export { useLocalStorage } from "./useLocalStorage";
-export { LocalStorageKeys } from "./useLocalStorage";
--- src/pages/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Home } from "./Home/Home";
-export { default as Favorites } from "./Favorites/Favorites";
-
